95813241e6a85dd2222318a7247add4224822e99
Patryk Steckel <patryksteckel@gmail.com>
2022-06-24 21:19:43 +0200

    MoveToBeacon -EvalCallback fixed

--- minigames/move_to_beacon/src/train.py
+++ minigames/move_to_beacon/src/train.py
@@ -14,12 +14,12 @@ FLAGS(sys.argv)
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
 env = MoveToBeaconEnv(step_mul=4)
-# eval_env = MoveToBeaconEnv(step_mul=8)
-# eval_path = "minigames/move_to_beacon/eval"
-# eval_callback = EvalCallback(eval_env, best_model_save_path=eval_path, log_path=eval_path,
-#                              eval_freq=10000, deterministic=False, render=False)
-eval_callback = None
+eval_env = MoveToBeaconEnv(step_mul=4)
+eval_path = "minigames/move_to_beacon/eval"
+eval_callback = EvalCallback(eval_env, best_model_save_path=eval_path, log_path=eval_path,
+                             eval_freq=100000, deterministic=False, render=False)
 
 model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="minigames/move_to_beacon/logs")
+# model = PPO.load("minigames/move_to_beacon/logs/PPO_8/model_731136.zip", env=env)
 model.learn(10000000, callback=eval_callback)
 
