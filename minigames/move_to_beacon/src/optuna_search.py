dad5836ce4d7912b11fd27ac095860c266b3f1f5
Patryk Steckel <patryksteckel@gmail.com>
2022-06-25 11:43:24 +0200

    MoveToBeacon -EvalCallback on the same env -Optuna search

---
+++ minigames/move_to_beacon/src/optuna_search.py
@@ -0,0 +1,247 @@
+from pprint import pprint
+from typing import Dict, Any, Union, Callable, Optional
+import os
+import pickle as pkl
+import gym
+import optuna
+from optuna.pruners import MedianPruner
+from optuna.samplers import TPESampler
+from optuna.visualization import plot_optimization_history, plot_param_importances
+from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnNoModelImprovement, BaseCallback
+from torch import nn as nn
+
+from minigames.move_to_beacon.src.env import MoveToBeaconEnv
+
+import sys
+from absl import flags
+FLAGS = flags.FLAGS
+FLAGS(sys.argv)
+
+
+study_path = "minigames/move_to_beacon/optuna/2"
+
+
+def linear_schedule(initial_value: Union[float, str]) -> Callable[[float], float]:
+    """
+    Linear learning rate schedule.
+
+    :param initial_value: (float or str)
+    :return: (function)
+    """
+    if isinstance(initial_value, str):
+        initial_value = float(initial_value)
+
+    def func(progress_remaining: float) -> float:
+        """
+        Progress will decrease from 1 (beginning) to 0
+        :param progress_remaining: (float)
+        :return: (float)
+        """
+        return progress_remaining * initial_value
+
+    return func
+
+
+def sample_ppo_params(trial: optuna.Trial) -> Dict[str, Any]:
+    """
+    Sampler for PPO hyperparams.
+
+    :param trial:
+    :return:
+    """
+    batch_size = trial.suggest_categorical("batch_size", [8, 16, 32, 64, 128, 256, 512])
+    n_steps = trial.suggest_categorical("n_steps", [8, 16, 32, 64, 128, 256, 512, 1024, 2048])
+    gamma = trial.suggest_categorical("gamma", [0.9, 0.95, 0.98, 0.99, 0.995, 0.999, 0.9999])
+    learning_rate = trial.suggest_loguniform("learning_rate", 1e-5, 1)
+    lr_schedule = "constant"
+    # Uncomment to enable learning rate schedule
+    # lr_schedule = trial.suggest_categorical('lr_schedule', ['linear', 'constant'])
+    ent_coef = trial.suggest_loguniform("ent_coef", 0.00000001, 0.1)
+    clip_range = trial.suggest_categorical("clip_range", [0.1, 0.2, 0.3, 0.4])
+    n_epochs = trial.suggest_categorical("n_epochs", [1, 5, 10, 20])
+    gae_lambda = trial.suggest_categorical("gae_lambda", [0.8, 0.9, 0.92, 0.95, 0.98, 0.99, 1.0])
+    max_grad_norm = trial.suggest_categorical("max_grad_norm", [0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 2, 5])
+    vf_coef = trial.suggest_uniform("vf_coef", 0, 1)
+    net_arch = trial.suggest_categorical("net_arch", ["small", "medium"])
+    # Uncomment for gSDE (continuous actions)
+    # log_std_init = trial.suggest_uniform("log_std_init", -4, 1)
+    # Uncomment for gSDE (continuous action)
+    # sde_sample_freq = trial.suggest_categorical("sde_sample_freq", [-1, 8, 16, 32, 64, 128, 256])
+    # Orthogonal initialization
+    ortho_init = False
+    # ortho_init = trial.suggest_categorical('ortho_init', [False, True])
+    # activation_fn = trial.suggest_categorical('activation_fn', ['tanh', 'relu', 'elu', 'leaky_relu'])
+    activation_fn = trial.suggest_categorical("activation_fn", ["tanh", "relu"])
+
+    # TODO: account when using multiple envs
+    if batch_size > n_steps:
+        batch_size = n_steps
+
+    if lr_schedule == "linear":
+        learning_rate = linear_schedule(learning_rate)
+
+    # Independent networks usually work best
+    # when not working with images
+    net_arch = {
+        "small": [dict(pi=[64, 64], vf=[64, 64])],
+        "medium": [dict(pi=[256, 256], vf=[256, 256])],
+    }[net_arch]
+
+    activation_fn = {"tanh": nn.Tanh, "relu": nn.ReLU, "elu": nn.ELU, "leaky_relu": nn.LeakyReLU}[activation_fn]
+
+    return {
+        "n_steps": n_steps,
+        "batch_size": batch_size,
+        "gamma": gamma,
+        "learning_rate": learning_rate,
+        "ent_coef": ent_coef,
+        "clip_range": clip_range,
+        "n_epochs": n_epochs,
+        "gae_lambda": gae_lambda,
+        "max_grad_norm": max_grad_norm,
+        "vf_coef": vf_coef,
+        # "sde_sample_freq": sde_sample_freq,
+        "policy_kwargs": dict(
+            # log_std_init=log_std_init,
+            net_arch=net_arch,
+            activation_fn=activation_fn,
+            ortho_init=ortho_init,
+        ),
+    }
+
+
+class TrialEvalCallback(EvalCallback):
+    """
+    Callback used for evaluating and reporting a trial.
+    """
+
+    def __init__(
+            self,
+            eval_env: gym.Env,
+            trial: optuna.Trial,
+            n_eval_episodes: int = 5,
+            eval_freq: int = 10000,
+            deterministic: bool = True,
+            verbose: int = 0,
+            best_model_save_path: Optional[str] = None,
+            log_path: Optional[str] = None,
+            callback_after_eval: Optional[BaseCallback] = None
+    ):
+
+        super().__init__(
+            eval_env=eval_env,
+            n_eval_episodes=n_eval_episodes,
+            eval_freq=eval_freq,
+            deterministic=deterministic,
+            verbose=verbose,
+            best_model_save_path=best_model_save_path,
+            log_path=log_path,
+            callback_after_eval=callback_after_eval
+        )
+        self.trial = trial
+        self.eval_idx = 0
+        self.is_pruned = False
+
+    def _on_step(self) -> bool:
+        continue_training = True
+        if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+            continue_training = super()._on_step()
+            self.eval_idx += 1
+            # report best or report current ?
+            # report num_timesteps or elapsed time ?
+            self.trial.report(self.last_mean_reward, self.eval_idx)
+            # Prune trial if needed
+            if self.trial.should_prune():
+                self.is_pruned = True
+                return False
+        return continue_training
+
+
+def objective(trial: optuna.Trial) -> float:
+    step_mul = trial.suggest_int("step_mul", 1, 16)
+    raw_resolution = trial.suggest_categorical("raw_resolution", [8, 16, 32, 64, 128])
+    env_kwargs = {"step_mul": step_mul, "raw_resolution": raw_resolution}
+
+    sampled_hyperparams = sample_ppo_params(trial)
+
+    path = f"{study_path}/trial_{str(trial.number)}"
+    os.makedirs(path, exist_ok=True)
+
+    env = MoveToBeaconEnv(**env_kwargs)
+    model = PPO("MlpPolicy", env=env, seed=None, verbose=0, **sampled_hyperparams)
+
+    stop_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=3, min_evals=5, verbose=1)
+    eval_callback = TrialEvalCallback(
+        env, trial, best_model_save_path=path, log_path=path,
+        n_eval_episodes=5, eval_freq=100000, deterministic=False, callback_after_eval=stop_callback
+    )
+
+    params = env_kwargs | sampled_hyperparams
+    with open(f"{path}/params.txt", "w") as f:
+        f.write(str(params))
+
+    try:
+        model.learn(10000000, callback=eval_callback)
+        env.close()
+    except (AssertionError, ValueError) as e:
+        env.close()
+        print(e)
+        print("============")
+        print("Sampled params:")
+        pprint(params)
+        raise optuna.exceptions.TrialPruned()
+
+    is_pruned = eval_callback.is_pruned
+    reward = eval_callback.best_mean_reward
+
+    del model.env
+    del model
+
+    if is_pruned:
+        raise optuna.exceptions.TrialPruned()
+
+    return reward
+
+
+if __name__ == "__main__":
+
+    sampler = TPESampler(n_startup_trials=0, multivariate=False)
+    pruner = MedianPruner(n_startup_trials=0)
+
+    study = optuna.create_study(
+        sampler=sampler,
+        pruner=pruner,
+        load_if_exists=True,
+        direction="maximize",
+    )
+
+    try:
+        study.optimize(objective, n_jobs=4, n_trials=100)
+    except KeyboardInterrupt:
+        pass
+
+    print("Number of finished trials: ", len(study.trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("Value: ", trial.value)
+
+    print("Params: ")
+    for key, value in trial.params.items():
+        print(f"    {key}: {value}")
+
+    study.trials_dataframe().to_csv(f"{study_path}/report.csv")
+
+    with open(f"{study_path}/study.pkl", "wb+") as f:
+        pkl.dump(study, f)
+
+    try:
+        fig1 = plot_optimization_history(study)
+        fig2 = plot_param_importances(study)
+
+        fig1.show()
+        fig2.show()
+    except (ValueError, ImportError, RuntimeError):
+        pass
--- minigames/move_to_beacon/src/train.py
+++ minigames/move_to_beacon/src/train.py
@@ -1,7 +1,7 @@
 from stable_baselines3 import PPO
 import logging
 
-from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.callbacks import EvalCallback, StopTrainingOnNoModelImprovement
 
 from minigames.move_to_beacon.src.env import MoveToBeaconEnv
 
@@ -13,13 +13,14 @@ FLAGS(sys.argv)
 
 logging.basicConfig(encoding='utf-8', level=logging.INFO)
 
-env = MoveToBeaconEnv(step_mul=4)
-eval_env = MoveToBeaconEnv(step_mul=4)
+env = MoveToBeaconEnv(step_mul=1)
 eval_path = "minigames/move_to_beacon/eval"
-eval_callback = EvalCallback(eval_env, best_model_save_path=eval_path, log_path=eval_path,
-                             eval_freq=100000, deterministic=False, render=False)
+stop_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=30, min_evals=50, verbose=1)
+eval_callback = EvalCallback(env, best_model_save_path=eval_path, log_path=eval_path,
+                             eval_freq=10000, deterministic=False, render=False,
+                             callback_after_eval=stop_callback)
 
-model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="minigames/move_to_beacon/logs")
-# model = PPO.load("minigames/move_to_beacon/logs/PPO_8/model_731136.zip", env=env)
+# model = PPO('MlpPolicy', env, verbose=1, tensorboard_log="minigames/move_to_beacon/logs")
+model = PPO.load("minigames/move_to_beacon/eval/best_model.zip", env=env)
 model.learn(10000000, callback=eval_callback)
 
