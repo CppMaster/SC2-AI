5f06c72f5e2fb3db6a6b55c475dd9bb10234898a
Patryk Steckel <patryksteckel@gmail.com>
2022-07-02 22:39:16 +0200

    CollectMineralsAndGas -derived observation -building scv -sending idle workers to minerals

--- minigames/colect_minerals_and_gas/src/env.py
+++ minigames/colect_minerals_and_gas/src/env.py
@@ -1,21 +1,43 @@
+import random
+from enum import IntEnum
 from typing import Optional, List
 
 import gym
-from gym.spaces import Dict, Box
+from gym.spaces import Box, MultiDiscrete
 from pysc2.env import sc2_env
 from pysc2.env.sc2_env import SC2Env
-from pysc2.lib import actions, features, units
+from pysc2.lib import actions, features
 import numpy as np
 import logging
 
-from pysc2.lib.features import Dimensions
+from pysc2.lib.features import Dimensions, Player, UnitLayer, FeatureUnit
+from pysc2.lib.units import Terran, Neutral
+
+
+class ActionIndex(IntEnum):
+    BUILD_SCV_1 = 0
+    BUILD_SCV_2 = 1
+    BUILD_CC = 2
+    BUILD_SUPPLY = 3
+    BUILD_REFINERY = 4
+
+
+class ObservationIndex(IntEnum):
+    MINERALS = 0    # scale 500
+    SUPPLY_TAKEN = 1    # scale 50
+    SUPPLY_ALL = 2      # scale 50
+    SUPPLY_FREE = 3     # scale 16
+    CC_BUILT = 4
+    SCV_COUNT = 5   # scale 50
+    REFINERY_COUNT = 6  # scale 4
+    IS_REFINERY_BUILDING = 7
 
 
 class CollectMineralAndGasEnv(gym.Env):
 
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, step_mul: int = 8, realtime: bool = False, resolution: int = 32):
+    def __init__(self, step_mul: int = 8, realtime: bool = False, resolution: int = 32, random_order=False):
         self.settings = {
             'map_name': "CollectMineralsAndGas",
             'players': [sc2_env.Agent(sc2_env.Race.terran)],
@@ -30,43 +52,89 @@ class CollectMineralAndGasEnv(gym.Env):
             'step_mul': step_mul
         }
         self.resolution = resolution
-        # TODO action_space and observation_space
-        self.action_space = Box(low=-0.5, high=0.5, shape=(4,))
-        self.observation_space = Dict({
-            "minerals": Box(low=-0.5, high=0.5, shape=(resolution, resolution)),
-            "marines": Box(low=-0.5, high=0.5, shape=(2, 2))
-        })
+        self.action_space = MultiDiscrete([2] * len(ActionIndex))
+        self.observation_space = Box(low=0.0, high=1.0, shape=(len(ObservationIndex), ))
         self.env: Optional[SC2Env] = None
         self.logger = logging.getLogger("CollectMineralsAndGas")
-        self.last_raw_obs = None
+        self.raw_obs = None
+        self.random_order = random_order
 
     def init_env(self):
         self.env = sc2_env.SC2Env(**self.settings)
 
     def step(self, action: Optional[np.ndarray] = None):
-        self.last_raw_obs = self.env.step(self.get_actions(action))[0]
-        derived_obs = self.get_derived_obs(self.last_raw_obs)
-        return derived_obs, self.last_raw_obs.reward, self.last_raw_obs.last(), {}
+        self.raw_obs = self.env.step(self.get_actions(action))[0]
+        derived_obs = self.get_derived_obs()
+        return derived_obs, self.raw_obs.reward, self.raw_obs.last(), {}
 
     def get_actions(self, action: np.ndarray) -> List:
-        mapped_actions = []
-        # TODO fill mapped_actions
+        mapped_actions = self.send_idle_workers_to_minerals()
+        if action[ActionIndex.BUILD_SCV_1]:
+            mapped_actions.append(self.build_scv(0))
+        if action[ActionIndex.BUILD_SCV_2]:
+            mapped_actions.append(self.build_scv(1))
+        '''
+        TODO fill mapped_actions
+        build cc
+        build refinery
+        build supply depot
+        '''
+        mapped_actions = list(filter(lambda x: x is not None, mapped_actions))
+        if self.random_order:
+            random.shuffle(mapped_actions)
         return mapped_actions
+
+    def build_scv(self, ccidx: int):
+        player = self.raw_obs.observation.player
+        if player[Player.food_cap] - player[Player.food_used] < 1:
+            return None
+
+        ccs = self.get_units(Terran.CommandCenter)
+        if ccidx >= len(ccs):
+            return None
+        ccs = sorted(ccs, key=lambda u: u["x"])
+        cc = ccs[ccidx]
+        if cc[FeatureUnit.order_length] == 0:
+            return actions.RAW_FUNCTIONS.Train_SCV_quick("now", ccs[ccidx].tag)
+        return None
+
+    def send_idle_workers_to_minerals(self) -> List:
+        idle_scvs = list(filter(lambda u: u[FeatureUnit.order_length] == 0, self.get_units(Terran.SCV)))
+        minerals = self.get_units(Neutral.MineralField)
+        mineral_positions = np.array([[mineral[FeatureUnit.x], mineral[FeatureUnit.y]] for mineral in minerals])
+        orders = []
+        for idle_scv in idle_scvs:
+            scv_position = np.array([idle_scv[FeatureUnit.x], idle_scv[FeatureUnit.y]])
+            mineral_idx = np.sum(np.power(mineral_positions - scv_position, 2), axis=1).argmin()
+            orders.append(actions.RAW_FUNCTIONS.Harvest_Gather_unit(
+                "now", idle_scv[FeatureUnit.tag], minerals[mineral_idx][FeatureUnit.tag]
+            ))
+        return orders
 
     def reset(self):
         if self.env is None:
             self.init_env()
 
-        self.last_raw_obs = self.env.reset()[0]
-        return self.get_derived_obs(self.last_raw_obs)
+        self.raw_obs = self.env.reset()[0]
+        return self.get_derived_obs()
 
-    def get_derived_obs(self, raw_obs):
-        # TODO return derived obs
-        pass
+    def get_derived_obs(self) -> np.ndarray:
+        obs = np.zeros(shape=self.observation_space.shape)
+        player = self.raw_obs.observation.player
+        obs[ObservationIndex.MINERALS] = player[Player.minerals] / 500
+        obs[ObservationIndex.SUPPLY_TAKEN] = player[Player.food_used] / 50
+        obs[ObservationIndex.SUPPLY_ALL] = player[Player.food_cap] / 50
+        obs[ObservationIndex.SUPPLY_FREE] = (player[Player.food_cap] - player[Player.food_used]) / 16
+        obs[ObservationIndex.CC_BUILT] = float(len(self.get_units(Terran.CommandCenter)) > 1)
+        obs[ObservationIndex.SCV_COUNT] = len(self.get_units(Terran.SCV)) / 50
+        obs[ObservationIndex.REFINERY_COUNT] = len(self.get_units(Terran.Refinery)) / 4
+        obs[ObservationIndex.IS_REFINERY_BUILDING] = float(any(
+            [refinery[UnitLayer.build_progress < 100] for refinery in self.get_units(Terran.Refinery)]
+        ))
+        return obs
 
-    @staticmethod
-    def get_units(raw_obs, unit_type: int):
-        return [unit for unit in raw_obs.observation.raw_units if unit.unit_type == unit_type]
+    def get_units(self, unit_type: int):
+        return [unit for unit in self.raw_obs.observation.raw_units if unit.unit_type == unit_type]
 
     def render(self, mode="human"):
         pass
