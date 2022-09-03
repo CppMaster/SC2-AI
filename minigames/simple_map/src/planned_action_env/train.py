30a32025b1d12a8b96aa6ec047ca8444d0fdb65a
Patryk Steckel <patryksteckel@gmail.com>
2022-09-03 11:05:20 +0200

    Planned action training

---
+++ minigames/simple_map/src/planned_action_env/train.py
@@ -0,0 +1,33 @@
+import logging
+import sys
+from absl import flags
+from pysc2.env.sc2_env import Difficulty
+from sb3_contrib import MaskablePPO, RecurrentPPO
+from stable_baselines3 import PPO
+
+from stable_baselines3.common.monitor import Monitor
+
+from torch import nn
+
+
+from minigames.simple_map.src.planned_action_env.env import PlannedActionEnv
+
+FLAGS = flags.FLAGS
+FLAGS(sys.argv)
+
+logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
+
+suffix = "init_diff-medium"
+output_path = f"minigames/simple_map/results/planned_action_logs/{suffix}"
+
+env = PlannedActionEnv(step_mul=4, difficulty=Difficulty.medium)
+
+model = MaskablePPO(
+    "MlpPolicy", env, verbose=1, tensorboard_log=output_path,
+    gamma=0.9999, policy_kwargs=dict(activation_fn=nn.LeakyReLU, ortho_init=True),
+    batch_size=64, learning_rate=5e-6, normalize_advantage=True, n_steps=1200
+)
+# model = MaskablePPO.load("minigames/simple_map/results/logs/maskable_stack-obs_build-cc/medium-to-medium-hard_ep34.zip", env)
+
+model.learn(10000000, reset_num_timesteps=True)
+model.save(f"{output_path}/last_model.zip")
