6bbea6d51efa3e607136abd711a4726c5c2eef6c
Patryk Steckel <patryksteckel@gmail.com>
2022-12-26 11:29:17 +0100

    -PlannedEnv difficulty scheduler train -CollectMineralShards run update

--- minigames/collect_mineral_shards/src/run.py
+++ minigames/collect_mineral_shards/src/run.py
@@ -1,18 +1,28 @@
+import numpy as np
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3 import PPO
 
 from minigames.collect_mineral_shards.src.env import CollectMineralShardsEnv
 
 import sys
 from absl import flags
+
+from minigames.collect_mineral_shards.src.env_stick import CollectMineralShardsStickEnv
+from minigames.collect_mineral_shards.src.repeat_action_until_reward_wrapper import RepeatActionUntilReward
+
 FLAGS = flags.FLAGS
 FLAGS(sys.argv)
 
 
 model = PPO.load("minigames/collect_mineral_shards/eval/best_model.zip")
-env = CollectMineralShardsEnv(step_mul=8, realtime=True)
+env = CollectMineralShardsStickEnv(step_mul=1, realtime=False)
+monitor_env = Monitor(env)
+env = RepeatActionUntilReward(monitor_env, frame_punish=0.1)
 
-obs = env.reset()
-done = False
-while not done:
-    action, _states = model.predict(obs)
-    obs, rewards, dones, info = env.step(action)
+while True:
+    done = False
+    obs = env.reset()
+    while not done:
+        action, _states = model.predict(obs)
+        obs, rewards, done, info = env.step(action)
+    print(f"Episode reward: {monitor_env.episode_returns[-1]},\tAverage reward: {np.mean(monitor_env.episode_returns)}")
--- minigames/simple_map/src/planned_action_env/train.py
+++ minigames/simple_map/src/planned_action_env/train.py
@@ -9,7 +9,7 @@ from stable_baselines3.common.monitor import Monitor
 
 from torch import nn
 
-
+from minigames.simple_map.src.planned_action_env.difficulty_scheduler import DifficultyScheduler
 from minigames.simple_map.src.planned_action_env.env import PlannedActionEnv
 from minigames.simple_map.src.planned_action_env.score_reward_wrapper import ScoreRewardShaper
 from minigames.simple_map.src.planned_action_env.supply_depot_reward_shaper import SupplyDepotRewardShaper
@@ -21,16 +21,18 @@ FLAGS(sys.argv)
 
 logging.basicConfig(encoding='utf-8', level=logging.DEBUG)
 
-suffix = "more-observation_finishing-move-07_finetune-terran"
+suffix = "difficulty_scheduler"
 output_path = f"minigames/simple_map/results/planned_action_logs/{suffix}"
 
-
-env = PlannedActionEnv(step_mul=4, difficulty=Difficulty.very_hard, enemy_race=Race.terran, time_to_finishing_move=0.7,
-                       reward_shapers=[ScoreRewardShaper(reward_diff=0.001, kill_factor=1.0, army_factor=1.0,
+env = PlannedActionEnv(step_mul=4, difficulty=Difficulty.harder, enemy_race=Race.terran,
+                       time_to_finishing_move=0.7,
+                       reward_shapers=[ScoreRewardShaper(reward_diff=0.0001, kill_factor=1.0, army_factor=1.0,
                                                          mined_factor=0.1, economy_factor=0.1),
                                        WorkerRewardShaper(reward_diff=0.001, optimal_reward=1.0, suboptimal_reward=0.1,
                                                           over_max_reward=-1.0)],
-                       free_supply_margin_factor=1.0, output_path=output_path)
+                       free_supply_margin_factor=1.0, output_path=output_path,
+                       difficulty_scheduler=DifficultyScheduler(min_mean_score=-.5, max_mean_score=0.5,
+                                                                n_scores_mean=6))
 env = StackObservationsActionRewardsWrapper(env, reward_scale=1.0,
                                             observation_value_stack_config=ValueStackConfig(1, [10]),
                                             action_value_stack_config=ValueStackConfig(10, [50]),
@@ -39,9 +41,9 @@ env = StackObservationsActionRewardsWrapper(env, reward_scale=1.0,
 # model = MaskablePPO(
 #     "MlpPolicy", env, verbose=1, tensorboard_log=output_path,
 #     gamma=0.9999, policy_kwargs=dict(activation_fn=nn.LeakyReLU, ortho_init=True),
-#     batch_size=64, learning_rate=3e-4, normalize_advantage=True, n_steps=2048
+#     batch_size=256, learning_rate=3e-4, normalize_advantage=True, n_steps=2 ** 16, n_epochs=10
 # )
-model = MaskablePPO.load(f"minigames/simple_map/results/planned_action_logs/more-observation_finishing-move-07/best_model_1.zip", env)
+model = MaskablePPO.load(f"{output_path}/last_model.zip", env)
 
-model.learn(10000000, reset_num_timesteps=True)
+model.learn(100000000, reset_num_timesteps=True)
 model.save(f"{output_path}/last_model.zip")
