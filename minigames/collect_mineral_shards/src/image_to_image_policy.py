e7aff2c4c1511ff176c86028375eb13247d85fac
Patryk Steckel <patryksteckel@gmail.com>
2023-02-23 20:23:54 +0100

    ImageToImagePolicy

--- minigames/collect_mineral_shards/src/image_to_image_policy.py
+++ minigames/collect_mineral_shards/src/image_to_image_policy.py
@@ -1,6 +1,12 @@
+from typing import Tuple
+
+import numpy as np
 import torch
-from stable_baselines3.common.distributions import CategoricalDistribution
+from stable_baselines3.common.utils import obs_as_tensor
 from torch import nn
+from torch.nn import functional as F
+from stable_baselines3.common.distributions import CategoricalDistribution
+from torch.optim import Adam
 
 
 class ImageToImagePolicy(nn.Module):
@@ -11,28 +17,24 @@ class ImageToImagePolicy(nn.Module):
         super().__init__()
         self.action_dim = grid_width * grid_height
         self.n_output_channels = n_output_channels
-        self.conv_layers = [nn.Conv2d(
+        self.conv_layers = nn.ModuleList([nn.Conv2d(
             in_channels=n_hidden_channels if i > 0 else n_input_channels,
             out_channels=n_hidden_channels if i < n_conv_layers - 1 else n_output_channels,
             kernel_size=kernel_size,
             padding="same"
-        ) for i in range(n_conv_layers)]
-        self.value_layers = [nn.Linear(
+        ) for i in range(n_conv_layers)])
+        self.value_layers = nn.ModuleList([nn.Linear(
             in_features=n_value_neurons if i > 0 else self.action_dim * n_hidden_channels,
             out_features=n_value_neurons if i < n_value_layers - 1 else 1
-        ) for i in range(n_value_layers)]
+        ) for i in range(n_value_layers)])
         self.value_features_layer = value_features_layer
         self.flatten_action = nn.Flatten(start_dim=1, end_dim=2)
         self.action_distribution = CategoricalDistribution(self.action_dim)
+        self.optimizer = Adam(self.parameters())
 
     def forward(self, obs: torch.Tensor, deterministic: bool = False) -> (torch.Tensor, torch.Tensor, torch.Tensor):
-        features = obs
-        for i in range(self.value_features_layer):
-            features = self.conv_layers[i](features)
-
-        latent_pi = features
-        for i in range(self.value_features_layer, len(self.conv_layers)):
-            latent_pi = self.conv_layers[i](latent_pi)
+        features = self.extract_features(obs)
+        latent_pi = self.get_latent_pi(features)
         latent_pi_flattened = self.flatten_action(latent_pi)
         distributions = [self.action_distribution.proba_distribution(latent_pi_flattened[:, :, i])
                          for i in range(self.n_output_channels)]
@@ -41,10 +43,63 @@ class ImageToImagePolicy(nn.Module):
         actions = torch.stack(actions, -1)
         log_probs = torch.stack(log_probs, -1)
 
-        values = torch.flatten(features, start_dim=1)
-        for i in range(len(self.value_layers)):
-            values = self.value_layers[i](values)
+        values = self.get_values(features)
 
         return actions, values, log_probs
 
+    def reset_noise(self, n_envs: int = 1) -> None:
+        pass
 
+    def extract_features(self, obs: torch.Tensor) -> torch.Tensor:
+        features = obs
+        for i in range(self.value_features_layer):
+            features = self.conv_layers[i](features)
+            features = F.leaky_relu(features)
+        return features
+
+    def get_values(self, features: torch.Tensor) -> torch.Tensor:
+        values = torch.flatten(features, start_dim=1)
+        for i in range(len(self.value_layers)):
+            values = self.value_layers[i](values)
+            if i < len(self.value_layers) - 2:
+                values = F.leaky_relu(values)
+        return values
+
+    def get_latent_pi(self, features: torch.Tensor) -> torch.Tensor:
+        latent_pi = features
+        for i in range(self.value_features_layer, len(self.conv_layers)):
+            latent_pi = self.conv_layers[i](latent_pi)
+        return latent_pi
+
+    def evaluate_actions(self, obs: torch.Tensor, actions: torch.Tensor) \
+            -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        features = self.extract_features(obs)
+        latent_pi = self.get_latent_pi(features)
+        latent_pi_flattened = self.flatten_action(latent_pi)
+        distributions = [self.action_distribution.proba_distribution(latent_pi_flattened[:, :, i])
+                         for i in range(self.n_output_channels)]
+        log_probs = [distributions[i].log_prob(actions[:, i]) for i in range(self.n_output_channels)]
+        entropies = [distribution.entropy() for distribution in distributions]
+        log_probs = torch.stack(log_probs, -1)
+        entropies = torch.stack(entropies, -1)
+
+        values = self.get_values(features)
+
+        return values, log_probs, entropies
+
+    def set_training_mode(self, mode: bool) -> None:
+        """
+        Put the policy in either training or evaluation mode.
+
+        This affects certain modules, such as batch normalisation and dropout.
+
+        :param mode: if true, set to training mode, else set to evaluation mode
+        """
+        self.train(mode)
+
+    def obs_to_tensor(self, observation: np.ndarray) -> Tuple[torch.Tensor, bool]:
+        observation = obs_as_tensor(observation, self.device)
+        return observation, False
+
+    def predict_values(self, obs: torch.Tensor) -> torch.Tensor:
+        return self.get_values(self.extract_features(obs))
